5d15b41975e5d1dbfe30f1cb7f982e968bb9f1d6
Farhan Rafi <mfas7rafi@gmail.com>
2025-01-06 01:43:51 +0600

    set up clean architecture, added some entities

---
+++ src/EMS.Domain/Common/BaseNumberEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Domain.Common
+{
+    public class BaseNumberEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
